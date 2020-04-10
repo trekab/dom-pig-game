06c1955e4ccca5b3076e08408e5a36d19a306025
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-10 23:25:59 +0300

    Add logic for the hold button

--- app.js
+++ app.js
@@ -32,17 +32,28 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		roundScore += dice;
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-
-		document.getElementById('current-0').textContent = '0'; 
-		document.getElementById('current-1').textContent = '0';
-
-		//document.querySelector('.player-0-panel').classList.remove('active');
-		//document.querySelector('.player-1-panel').classList.add('active');
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-
-		document.querySelector('.dice').style.display = 'none';
+		switchPlayer();
 	}
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	scores[activePlayer] += roundScore;
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	switchPlayer();
+});
+
+function switchPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0'; 
+	document.getElementById('current-1').textContent = '0';
+
+	//document.querySelector('.player-0-panel').classList.remove('active');
+	//document.querySelector('.player-1-panel').classList.add('active');
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	document.querySelector('.dice').style.display = 'none';
+}
