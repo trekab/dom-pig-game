3d2b4fbbd9b6018b8f88a105be271f45a81b74d4
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-10 23:02:12 +0300

    Change player when a 1 in selected on the dice and update the current value

--- app.js
+++ app.js
@@ -13,17 +13,34 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0'; 
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0'; 
 document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	var dice = Math.floor(Math.random() * 6) + 1;
 
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
+
+	if (dice !== 1) {
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0'; 
+		document.getElementById('current-1').textContent = '0';
+
+		//document.querySelector('.player-0-panel').classList.remove('active');
+		//document.querySelector('.player-1-panel').classList.add('active');
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+	}
 });
